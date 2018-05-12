0c736b7ccfd0d24bc76296017d957c6e9df4bc1e
SHANG ONG <shangong@SHANGs-MacBook-Pro.local>
2018-05-11 14:27:14 -1000

    completed exercise 2

--- exercises.py
+++ exercises.py
@@ -4,7 +4,8 @@
 
 #1 
 #Declare two variables box_1 and box_2 and assign any number value of your choosing. These two variables will be used to invoke functions #2 - #5.
-
+box_1 = 5
+box_2 = 10
 
 #2
 #Function - add
@@ -13,6 +14,13 @@
 #This function will add two numbers (the parameters, `num_1` and `num_2`) and return the sum of these two numbers. Invoke the function and assign it to a variable named `sum`.
 
 #Print the sum variable to see your result.
+
+def add(num_1, num_2):
+    return num_1 + num_2
+
+sum = add(box_1, box_2)
+print(sum)
+
 
 
 #3
